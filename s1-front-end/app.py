7f379023b4ea49d34834a746227c37fc2b529354
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-08 10:37:52 +0000

    s2 doesn't generate anything random but there are some services to get a pipeline going

--- s1-front-end/app.py
+++ s1-front-end/app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    response = requests.get('http://oldgypsy_constilations_1:5002/get/constalation')
-    card = requests.get('http://oldgypsy_tarot-cards_1:5003/get/card')
-    return "Constalation " + response.text + "\n" + "Card " + card.text
+    response = requests.get("http://oldgypsy_constilations_1:5002/get/constalation")
+    card = requests.get("http://oldgypsy_tarot-cards_1:5003/get/card")   
+    premonition = requests.get("http://oldgypsy_combination_1:5001/get/premonition/a/2/b/3")
+    return response.text + "\n" + card.text + premonition.text
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, host='0.0.0.0')
+    app.run(port=5000, debug=True, host="0.0.0.0")
