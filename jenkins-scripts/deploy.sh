2d4b37d7d33eb6832460897dbf2e9d3f03dd1f04
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-09 15:07:12 +0000

    quick commit test

--- jenkins-scripts/deploy.sh
+++ jenkins-scripts/deploy.sh
@@ -1,5 +1,4 @@
 scp ./docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
-scp ./nginx/nginx.conf jenkins@swarm-master:/home/jenkins/nginx/nginx.conf
 ssh jenkins@swarm-master << EOF
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml micro-service
 EOF
