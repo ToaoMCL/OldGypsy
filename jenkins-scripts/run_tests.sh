1a4ded4735fd0bcc53665cd24c5426aa68509c34
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-09 14:24:45 +0000

    quick commit test

--- jenkins-scripts/run_tests.sh
+++ jenkins-scripts/run_tests.sh
@@ -1,11 +1,25 @@
 sudo apt-get update
 sudo apt-get install -y python3-venv python3-pip
 
-cd s1-front-end
+#cd s1-front-end
 python3 -m venv venv
 source venv/bin/activate
-pip3 isntall -r requirements.txt
+# Run service 1 front end tests
+pip3 install -r s1-front-end/requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
-python3 -m pytest --cov=s1-front-end --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov-config=.coveragerc --cov=s1-front-end --cov-report term-missing --junitxml junit.xml
+
+# Run service 2 tests
+pip3 install -r s2-combination/requirements.txt
+python3 -m pytest --cov-config=.coveragerc --cov=s2-combination --cov-report term-missing --junitxml junit.xml
+
+# Run service 3 tests
+pip3 install -r s3-constalations/requirements.txt
+python3 -m pytest --cov-config=.coveragerc --cov=s3-constalations --cov-report term-missing --junitxml junit.xml
+
+# Run service 4 tests
+pip3 install -r s4-tarot-cards/requirements.txt
+python3 -m pytest --cov-config=.coveragerc --cov=s4-tarot-cards --cov-report term-missing --junitxml junit.xml
+
 deactivate
 cd ..
