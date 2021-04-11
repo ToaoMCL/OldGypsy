2d501d974e373b445ec201a7034dfb540bc60b9b
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-11 12:44:57 +0000

    quick commit test

--- jenkins-scripts/run_tests.sh
+++ jenkins-scripts/run_tests.sh
@@ -5,7 +5,7 @@ cd s1-front-end
 python3 -m venv venv
 source venv/bin/activate
 # Run service 1 front end tests
-pip3 install -r s1-front-end/requirements.txt
+pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov-config=.coveragerc --cov=s1-front-end --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -15,7 +15,7 @@ cd ..
 cd s2-combination
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r s2-combination/requirements.txt
+pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov-config=.coveragerc --cov=s2-combination --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -25,7 +25,7 @@ cd ..
 cd s3-constalations
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r s3-constalations/requirements.txt
+pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov-config=.coveragerc --cov=s3-constalations --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -35,7 +35,7 @@ cd ..
 cd s4-tarot-cards
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r s4-tarot-cards/requirements.txt
+pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov-config=.coveragerc --cov=s4-tarot-cards --cov-report term-missing --junitxml junit.xml
 deactivate
