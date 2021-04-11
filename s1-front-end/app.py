843fa296570324a538c8a170e6ba880df52ea0fd
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-11 12:29:32 +0000

    quick commit test

--- s1-front-end/app.py
+++ s1-front-end/app.py
@@ -13,26 +13,20 @@ db = SQLAlchemy(app)
 class Predictions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-
-db.create_all()
-newent = Template_Table(name="new entry")
-db.session.add(newent)
-db.session.commit()
-
-#response_list = []
+'''
 response_string = ""
 response = db.session.query(Template_Table).all()
 for i in response:
     response_string = response_string + str(i.id) + "," + i.name + "\n"
-    #response_list.append((i.id, i.name)) 
-
+    
+'''
 
 @app.route("/", methods=["GET"])
 def home():
     card = requests.get("http://tarot-cards:5003/get/card")   
     constalation = requests.get("http://constilations:5002/get/constalation")
     premonition = requests.get("http://combination:5001/get/premonition/a/2/b/3")
-    return constalation.text + "\n" + card.text + premonition.text + "\n" + response_string
+    return constalation.text + "\n" + card.text + premonition.text + "\n"# + response_string
 
 
 if __name__ == "__main__":
