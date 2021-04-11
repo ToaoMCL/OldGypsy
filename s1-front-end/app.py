ed056f9314643f0e894f1b25d015c81008cfae5e
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-11 16:44:43 +0000

    testing requests

--- s1-front-end/app.py
+++ s1-front-end/app.py
@@ -25,9 +25,9 @@ for i in response:
 def home():
     card = requests.get("http://tarot-cards:5003/get/card")   
     constalation = requests.get("http://constilations:5002/get/constalation")
-    result = card.text
+    result = jsonify(card.text)
    # premonition = requests.post("http://combination:5001/get/premonition/a/2/b/3", json=)
-    return result #constalation.text + "\n" + card.text + premonition.text + "\n" + os.getenv("app_version")# + response_string
+    return result["card_name"] #constalation.text + "\n" + card.text + premonition.text + "\n" + os.getenv("app_version")# + response_string
 
 
 if __name__ == "__main__":
