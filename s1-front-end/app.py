a13edc644966b94413eff7e2f5f118b536ac3919
Eryk Pabianczyk <erykmcl@gmail.com>
2021-04-11 17:30:03 +0000

    testing requests

--- s1-front-end/app.py
+++ s1-front-end/app.py
@@ -27,8 +27,8 @@ def home():
     constalation = requests.get("http://constilations:5002/get/constalation")
     result = card.text
     print(card.json)
-    r2 = dict(result)
-    a = result["card_name"]
+    #r2 = dict(result)
+    #a = result["card_name"]
     #premonition = requests.post("http://combination:5001/get/premonition/a/2/b/3", json=)
     return "a" #constalation.text + "\n" + card.text + premonition.text + "\n" + os.getenv("app_version")# + response_string
 
